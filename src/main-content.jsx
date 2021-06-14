ab4e6eeb08a3839073796146a4ecb0f9f07d42e2
L'eau Artésienne <spamies@live.com>
2021-06-14 11:50:25 +0100

    Delete src directory

--- src/main-content.jsx
+++
@@ -1,9 +0,0 @@
-export default function MainContent () {
-  return (
-    <div className="bg-main py-12 md:py-20 text-white min-h-screen">
-      <div className="mx-2 sm:mx-4 md:mx-8 lg:mx-16 xl:mx-30 2xl:mx-40 space-y-4">
-        {new Array(1000).fill().map((v, i) => <p key={i}>{i}</p>)}
-      </div>
-    </div>
-  )
-}
